084f008423e5cad577462a9ba9aaa9432f7eb400
peterudu <puduevborho7@gmail.com>
2023-12-20 16:29:44 +0000

    Create CommentDetailSerializer which references Post id

--- comments/serializers.py
+++ comments/serializers.py
@@ -5,6 +5,7 @@ from .models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     """
     Serializer for Comment model
+    Three extra fields are added when Comment instances are returned
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
@@ -22,3 +23,9 @@ class CommentSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image','post',
             'created_at', 'updated_at', 'content'
         ]
+
+class CommentDetailSerializer(CommentSerializer):
+    """
+    Serializer for Comment model applied in Detail view
+    """
+    post = serializers.ReadOnlyField(source='post.id')
