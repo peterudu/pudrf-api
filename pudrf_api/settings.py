a6c76a9a94141516426dfec32d8198970464d4c9
peterudu <puduevborho7@gmail.com>
2024-02-19 17:13:01 +0000

    Show list of posted comments

--- pudrf_api/settings.py
+++ pudrf_api/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m!_#)u8yf-e($1i3$i9ja-ap-rcg96nsi$f++^5$sk+*v5v0er'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
