131fe96f498f1098f172c96a83cca13b1dc189df
peterudu <puduevborho7@gmail.com>
2024-02-24 08:59:20 +0000

    Set rest environment variables for deployment

--- pudrf_api/settings.py
+++ pudrf_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env
@@ -56,10 +57,10 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-peterudu-pudrfapi-yk89b4dn1gm.ws-eu108.gitpod.io']
-
+# ALLOWED_HOSTS = ['8000-peterudu-pudrfapi-yk89b4dn1gm.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['greencontributionsapi.herokuapp.com', 'localhost']
 
 # Application definition
 
@@ -80,7 +81,8 @@ INSTALLED_APPS = [
     'allauth', 
     'allauth.account', 
     'allauth.socialaccount', 
-    'dj_rest_auth.registration', 
+    'dj_rest_auth.registration',
+    'corsheaders', 
 
     'profiles',
     'posts',
@@ -90,6 +92,7 @@ INSTALLED_APPS = [
 ]
 SITE_ID = 1
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -98,6 +101,19 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ] 
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.gitpod\.io$",
+    ] 
+
+CORS_ALLOW_CREDENTIALS = True
+
+JWT_AUTH_SAMESITE = 'None'
 
 ROOT_URLCONF = 'pudrf_api.urls'
 
@@ -124,10 +140,14 @@ WSGI_APPLICATION = 'pudrf_api.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default': ({
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    if 'DEV' in os.environ 
+    else dj_database_url.parse(
+        os.environ.get('DATABASE_URL')
+    ))
 }
 
 
